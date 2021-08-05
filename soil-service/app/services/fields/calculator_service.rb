db66000885fc9d9a9a76488b9917bf26895bca58
Ievgenii Kravchuk <kravchuk.ievgenii@gmail.com>
2021-08-05 17:57:32 +0200

    Fields::CalculatorService#humus_balance works

--- soil-service/app/services/fields/calculator_service.rb
+++ soil-service/app/services/fields/calculator_service.rb
@@ -2,6 +2,7 @@
 
 module Fields
   class CalculatorService
+    CONS_COEF = 1.3
     attr_reader :crop_ids
 
     def initialize(crop_ids)
@@ -9,7 +10,29 @@ module Fields
     end
 
     def humus_balance
-      nil
+      crop_ids.each_with_index.sum do |id, index|
+        consecutive?(index) ? CONS_COEF * humus_delta_by_id(id) : humus_delta_by_id(id)
+      end
+    end
+
+    private
+
+    def humus_delta_by_id(id)
+       crop_by_id(id)[:humus_delta]
+    end
+
+    def crop_by_id(id)
+      all_crops.find { |crop| crop[:value] == id.to_i }
+    end
+
+    def consecutive?(index)
+      return false if index == 0
+
+      crop_ids[index] == crop_ids[index - 1]
+    end
+
+    def all_crops
+      @all_crops ||= CropsService.instance.fetch_all_crops
     end
   end
 end
--- soil-service/spec/services/fields/calculator_service_spec.rb
+++ soil-service/spec/services/fields/calculator_service_spec.rb
@@ -3,6 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe Fields::CalculatorService do
+  let(:crop_ids) { [1, 1, 2] }
+
   describe '#humus_balance' do
     subject(:humus_balance) { described_class.new(crop_ids).humus_balance }
 
@@ -19,6 +21,58 @@ RSpec.describe Fields::CalculatorService do
 
       it 'returns correct value' do
         is_expected.to eq(-5.6)
+      end
+    end
+  end
+
+  describe '#consecutive?' do
+    subject(:consecutive?) { described_class.new(crop_ids).send(:consecutive?, index) }
+
+    context 'for index eq 0' do
+      let(:index) { 0 }
+
+      it 'returns false' do
+        is_expected.to be_falsey
+      end
+    end
+
+    context 'for index eq 1' do
+      let(:index) { 1 }
+
+      it 'returns true' do
+        is_expected.to be_truthy
+      end
+    end
+
+    context 'for index eq 2' do
+      let(:index) { 2 }
+
+      it 'returns false' do
+        is_expected.to be_falsey
+      end
+    end
+  end
+
+  describe '#humus_delta_by_id' do
+    subject(:humus_delta_by_id) { described_class.new(crop_ids).send(:humus_delta_by_id, id) }
+
+    context 'for id eq 1' do
+      let(:id) { 1 }
+
+      it 'returns correct value' do
+        is_expected.to eq -2
+      end
+    end
+  end
+
+  describe '#crop_by_id' do
+    subject(:crop_by_id) { described_class.new(crop_ids).send(:crop_by_id, id) }
+
+    context 'for id eq 2' do
+      let(:id) { 2 }
+
+      it 'returns correct crop' do
+        is_expected.to eq({ humus_delta: -1, label: 'Winter Wheat', value: 2 })
       end
     end
   end
