fdb6244a2faa4118cff80d69c6931008039354ef
Ievgenii Kravchuk <kravchuk.ievgenii@gmail.com>
2021-08-05 16:01:51 +0200

    Added route/action for humus_balance

--- soil-service/app/controllers/fields_controller.rb
+++ soil-service/app/controllers/fields_controller.rb
@@ -1,5 +1,9 @@
-class FieldsController < ActionController::Base
+class FieldsController < ApplicationController
   def index
     render json: FieldsService.instance.fetch_fields
+  end
+
+  def humus_balance
+    render json: { humus_balance: nil }
   end
 end
--- soil-service/config/routes.rb
+++ soil-service/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :fields, only: [:index]
+  resources :fields, only: [:index] do
+    collection do
+      get 'humus_balance'
+    end
+  end
+
   resources :crops, only: [:index]
 end
